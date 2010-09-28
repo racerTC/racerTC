92378649385c4916b7abfe9c6e7383c864276d13
pawegio <pawegio@87b17f88-21ed-483e-9168-594dc2f48b01>
2010-09-28 19:43:58 +0000

    Dodana klasa RacerMenu oraz RacerCarMenu + zaktualizowany layout pionowy w głównym menu.

---
+++ src/com/games/racertc/RacerCarMenu.java
@@ -0,0 +1,21 @@
+package com.games.racertc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class RacerCarMenu extends Activity implements OnClickListener{
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+    }
+	
+	public void onClick(View v) {
+    	
+    }
+}
---
+++ src/com/games/racertc/RacerMenu.java
@@ -0,0 +1,37 @@
+package com.games.racertc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class RacerMenu extends Activity implements OnClickListener{
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        View singlePlayerButton = findViewById(R.id.single_player);
+        singlePlayerButton.setOnClickListener(this);
+        View multiplayerButton = findViewById(R.id.multiplayer);
+        multiplayerButton.setOnClickListener(this);
+        View optionsButton = findViewById(R.id.options);
+        optionsButton.setOnClickListener(this);
+        View exitGameButton = findViewById(R.id.exit_game);
+        exitGameButton.setOnClickListener(this);
+    }
+	
+	public void onClick(View v) {
+    	switch(v.getId()) {
+    	case R.id.single_player:
+    		Intent i = new Intent(this, RacerGame.class);
+    		startActivity(i);
+    		break;
+    	case R.id.exit_game:
+    		finish();
+    		break;
+    	}
+    }
+}
